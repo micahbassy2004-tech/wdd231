f959fa497fa2412b78c7bc530ca91cf5a09bef48
YourGitHubUsername <your-email@example.com>
2026-05-30 20:17:17 +0100

    Fix join page audit issues

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,1 +1,16 @@
+// Set timestamp when page loads
 document.querySelector("#timestamp").value = new Date().toISOString();
+
+// Open modals
+document.querySelectorAll("[data-modal]").forEach(button => {
+  button.addEventListener("click", () => {
+    document.getElementById(button.dataset.modal).showModal();
+  });
+});
+
+// Close modals
+document.querySelectorAll("[data-close]").forEach(button => {
+  button.addEventListener("click", () => {
+    document.getElementById(button.dataset.close).close();
+  });
+});
